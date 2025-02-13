9b3af946af5d697d0ca889353cba777eebf9bb87
Victor Contreras <contreras3991@gmail.com>
2025-02-13 15:39:20 -0300

    fix test and app.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,5 @@
 # from .models import
-from http import HTTPStatus
-
 from fastapi import FastAPI
-
-from backend.schemas import User
 
 app = FastAPI()
 
@@ -11,8 +7,3 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return {'Hello': 'World'}
-
-
-@app.post('/', status_code=HTTPStatus.CREATED)
-def create_user(user: User) -> None:
-    return {'name': user.name, 'last_name': user.last_name}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from backend.main import app
-
-
-def test_root_should_return_hello_world():
-    client = TestClient(app)
-
+def test_root_should_return_hello_world(client):
     response = client.get('/')
 
     assert response.json() == {'Hello': 'World'}
