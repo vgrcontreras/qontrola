9b706088b809aa7189baf6b74fae6f67e954c8fe
Victor Contreras <contreras3991@gmail.com>
2025-03-03 21:33:44 -0300

    feat: add put and delete endpoint for department route

--- backend/src/routes/departments.py
+++ backend/src/routes/departments.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from src.database import get_session
@@ -11,6 +12,7 @@ from src.schemas import (
     DepartmentPublic,
     DepartmentPublicList,
     DepartmentSchema,
+    Message,
 )
 
 router = APIRouter(prefix='/departments', tags=['departments'])
@@ -63,3 +65,53 @@ def read_all_departments(session: T_Session):
     db_departments = session.scalars(select(Department)).all()
 
     return {'departments': db_departments}
+
+
+@router.delete(
+    '/{department_name}', status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_department(department_name: str, session: T_Session):
+    db_department = session.scalar(
+        select(Department).where(Department.name == department_name)
+    )
+
+    if db_department is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Department not found'
+        )
+
+    session.delete(db_department)
+    session.commit()
+
+    return {'message': 'Department deleted'}
+
+
+@router.put(
+    '/{department_id}',
+    status_code=HTTPStatus.OK,
+    response_model=DepartmentPublic,
+)
+def update_department(
+    department_id: int, department: DepartmentSchema, session: T_Session
+):
+    db_department = session.scalar(
+        select(Department).where(Department.id == department_id)
+    )
+
+    if db_department is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Department not found'
+        )
+
+    try:
+        db_department.name = department.name
+
+        session.commit()
+        session.refresh(db_department)
+
+        return db_department
+
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail='Department already exists'
+        )
