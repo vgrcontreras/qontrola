c6e57f693ea3dc1b3af5f832e38f42aef55f9f6b
Victor Contreras <contreras3991@gmail.com>
2025-03-03 21:31:18 -0300

    :test_tube: test: add tests for departments route

--- backend/tests/test_department.py
+++
@@ -1,8 +0,0 @@
-from http import HTTPStatus
-
-
-def test_create_department(client):
-    response = client.post('/departments/', json={'name': 'test_department'})
-
-    assert response.status_code == HTTPStatus.CREATED
-    assert response.json() == {'id': 1, 'name': 'test_department'}
