12cef514b678e3b9c76d49392ff606dc78dd967f
Victor Contreras <contreras3991@gmail.com>
2025-03-05 19:14:56 -0300

    feat: create users routes

---
+++ backend/src/routes/users.py
@@ -0,0 +1,91 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+
+from src.database import get_session
+from src.models import User
+from src.schemas.base import Message
+from src.schemas.users import (
+    UserList,
+    UserPublic,
+    UserPublicSalary,
+    UserSchema,
+    UserUpdate,
+)
+
+router = APIRouter(prefix='/users', tags=['users'])
+
+T_Session = Annotated[Session, Depends(get_session)]
+
+
+@router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema, session: T_Session):
+    db_user = session.scalar(select(User).where(User.email == user.email))
+
+    if db_user is not None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail='User already exists'
+        )
+
+    user_attrs = user.model_dump()
+
+    new_user = User(**user_attrs)
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+    return new_user
+
+
+@router.get('/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users(session: T_Session):
+    db_users = session.scalars(select(User))
+
+    return {'users': db_users}
+
+
+@router.delete('/{user_id}', status_code=HTTPStatus.OK, response_model=Message)
+def delete_user(user_id: int, session: T_Session):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail='User Not Found'
+        )
+
+    session.delete(db_user)
+    session.commit()
+
+    return {'message': 'User deleted'}
+
+
+@router.patch(
+    '/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublicSalary
+)
+def update_user(user_id: int, user: UserUpdate, session: T_Session):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail='User Not Found'
+        )
+
+    try:
+        for key, value in user.model_dump(exclude_unset=True).items():
+            setattr(db_user, key, value)
+
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+        return db_user
+
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail='User email already exists'
+        )
