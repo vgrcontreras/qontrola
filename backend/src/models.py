dbd12084f87316a7ad37a96fe6ade3cbd5310d19
Victor Contreras <contreras3991@gmail.com>
2025-03-05 19:10:20 -0300

    feat: create User SQLAlchemy model

--- backend/src/models.py
+++ backend/src/models.py
@@ -18,3 +18,22 @@ class Department:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, onupdate=func.now(), nullable=True
     )
+
+
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    salary: Mapped[float] = mapped_column(nullable=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, onupdate=func.now(), nullable=True
+    )
