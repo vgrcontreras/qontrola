25b6a2a34f816a855cc2f5293374dc7d06ef7d43
Victor Contreras <contreras3991@gmail.com>
2025-03-08 13:13:26 -0300

    fix: replace constant values for env variables

--- backend/tests/test_security.py
+++ backend/tests/test_security.py
@@ -1,13 +1,16 @@
 from jwt import decode
 
-from src.security import ALGORITHM, SECRET_KEY, create_access_token
+from src.security import create_access_token
+from src.settings import settings
 
 
 def test_jwt():
     data = {'test': 'test'}
     token = create_access_token(data)
 
-    decoded = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    decoded = decode(
+        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
 
     assert decoded['test'] == data['test']
     assert 'exp' in decoded
