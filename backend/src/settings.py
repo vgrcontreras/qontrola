0cd8bd6bb47e49c144852ea48fd1b4b68d41f875
Victor Contreras <contreras3991@gmail.com>
2025-03-07 23:36:36 -0300

    add new env variables in pydating settings module

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
         env_file='.env', env_file_encoding='utf-8'
     )
     DATABASE_URL: str = 'sqlite:///./dev.db'
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
 
 settings = Settings()
