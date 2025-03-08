e848cea48e4cecd19b5a38d53e32880bc0a6c97a
Victor Contreras <contreras3991@gmail.com>
2025-03-07 23:32:59 -0300

    feat: create token endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,22 @@
 # from .models import
 from http import HTTPStatus
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from src.database import get_session
+from src.models import User
 from src.routes import departments, users
 from src.schemas.base import Message
+from src.schemas.token import Token
+from src.security import create_access_token, verify_password
 
 app = FastAPI()
+
+T_Session = Annotated[Session, Depends(get_session)]
 
 app.include_router(departments.router)
 app.include_router(users.router)
@@ -15,3 +25,28 @@ app.include_router(users.router)
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/token', status_code=HTTPStatus.OK, response_model=Token)
+def login_for_access_token(
+    session: T_Session, form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user_db = session.scalar(
+        select(User).where(User.email == form_data.username)
+    )
+
+    if not user_db:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user_db.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user_db.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
