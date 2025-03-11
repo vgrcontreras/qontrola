e848dc0319efb1e4fc0e92f6a5a2f00cd9c9399b
Victor Contreras <contreras3991@gmail.com>
2025-03-10 21:14:24 -0300

    feat(api): include clients router

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from src.api.routes import departments, login, users
+from src.api.routes import clients, departments, login, users
 
 app = FastAPI()
 
 app.include_router(departments.router)
 app.include_router(users.router)
 app.include_router(login.router)
+app.include_router(clients.router)
